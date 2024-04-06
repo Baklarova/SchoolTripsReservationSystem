ab66eef5ff9b27a0af522c1ba0d32a1ca40ef316
Rada Baklarova <rea2@abv.bg>
2024-04-06 20:40:03 +0300

    Add project files.

---
+++ SchoolTripsReservationSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchoolTripsReservationSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
