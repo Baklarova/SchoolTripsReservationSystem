ffed60a36c41c74a68d0c8e049ac795dd664c584
Rada Baklarova <rea2@abv.bg>
2024-04-16 20:39:10 +0300

    Custom error pages

--- SchoolTripsReservationSystem/Program.cs
+++ SchoolTripsReservationSystem/Program.cs
@@ -23,12 +23,13 @@ namespace SchoolTripsReservationSystem
 
             if (app.Environment.IsDevelopment())
             {
+                app.UseDeveloperExceptionPage();
                 app.UseMigrationsEndPoint();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                
+                app.UseExceptionHandler("/Home/Error/500");
+                app.UseStatusCodePagesWithRedirects("/Home/Error?statusCode={0}");                
                 app.UseHsts();
             }
 
